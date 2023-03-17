c1a542fe9ed5a6eb2f495e76fee613a97941b077
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-17 20:01:30 +0600

    Sorting limited cards via dates done

--- js/index.js
+++ js/index.js
@@ -22,3 +22,31 @@ document.getElementById('seeMoreButton').addEventListener('click',function(){
     }
   })
 
+  // Sorting limited cards via dates
+
+document.getElementById('sort-btn').addEventListener('click',function(){
+
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res => res.json())
+    .then(datas => sortByDate(datas.data.tools))
+    .catch(error => {
+        alert('error found no data for loading')
+    })
+});
+
+
+function sortByDate(allData){
+
+  console.log(allData);
+
+  let sliceAllData = allData.slice(0,6);
+
+  console.log(sliceAllData);
+
+  sliceAllData.sort((a, b) => Date.parse(a.published_in) - Date.parse(b.published_in));
+
+  showData(sliceAllData,true);
+   
+}
+ 
+
